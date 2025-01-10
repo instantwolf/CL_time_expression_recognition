61922c3b09268033397a862700fa37ef2206968e
Wolfgang Prett <wolfi-92@gmx.net>
2025-01-10 14:48:16 +0100

    added some libs for heideltime and uwtime

---
+++ TemporalExpressionsCheck/ComparisonInstance.py
@@ -0,0 +1,31 @@
+class ComparisonInstance:
+        def __init__(self, solution_list, prediction_list):
+            """
+            Initialize the comparer with two TimexCollection objects.
+            :param collection1: The first TimexCollection (e.g., ground truth).
+            :param collection2: The second TimexCollection (e.g., predicted tags).
+            """
+            self.truth = set(solution_list)  # Ground truth
+            self.prediction = set(prediction_list)  # Predicted collection
+
+            self.result =
+
+        #truth and prediction are equal
+        def positive(self):
+            return self.truth == self.prediction
+
+        #truth > prediction -> prediction missing values, false negative
+        def false_negative(self):
+            return self.truth != self.prediction and self.truth.issuperset(self.prediction)
+
+        #truth < prediction -> predicted too many, false positive
+        def false_positive(self):
+            return self.truth != self.prediction and self.prediction.issuperset(self.truth)
+
+        def negative(self):
+            return not self.positive() and not self.false_negative() and not self.false_positive()
+
+
+        @staticmethod
+        def eval_instance(self):
+            
---
+++ TemporalExpressionsCheck/python_heideltime.py
@@ -0,0 +1,27 @@
+import dependencies/python_heideltime
+hw = HeidelTime.HeidelTimeWrapper('english')
+hw.parse('Neil Armstrong was born in 1930')
+
+ht = HeidelTime()
+
+# Configure Heideltime
+ht.set_language("english")
+#ht.set_treetagger_home("<path_to_treetagger>")  # If required
+#ht.set_document_type("narrative")
+#ht.set_output_format("timex")
+
+data = load_dataset_pht()
+
+for i in range(0,len(data)):
+    text = data[i]["text"]
+    datetime = data[i]["dct"]
+
+    ht.set_document_type('NEWS')
+    ht.set_document_time(datetime)
+    tagged_text = ht.parse(text)
+
+    print(f"Original Text: {text}")
+    print(f"Tagged Text: {tagged_text}")
+
+    if i < len(data) - 1:
+        input("\nPress Enter to display the next batch...")
